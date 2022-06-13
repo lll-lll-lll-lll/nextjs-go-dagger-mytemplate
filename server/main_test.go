6d9b80852fb85b4f9d6ede9024d5d2938ce7ad70
0xpeIpeI <hokumaru12@gmail.com>
2022-06-13 23:56:33 +0900

    [modify] testを削除

--- server/main_test.go
+++ server/main_test.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"io/ioutil"
-	"log"
 	"net/http"
 	"net/http/httptest"
-	"os"
 	"testing"
 
 	"github.com/gin-gonic/gin"
@@ -27,37 +25,3 @@ func TestHello(t *testing.T) {
     assert.Equal(t, http.StatusOK, w.Code)
 }
 
-
-func TestMain(m *testing.M){
-    
-    app.Initialize(
-        os.Getenv("APP_DB_USERNAME"),
-		os.Getenv("APP_DB_PASSWORD"),
-		os.Getenv("APP_DB_NAME"),
-    )
-    ensureTableExists()
-    code := m.Run()
-    clearTable()
-    os.Exit(code)
-}
-
-
-func ensureTableExists() {
-    if _, err := app.DB.Exec(tableCreationQuery); err != nil {
-        log.Fatal(err)
-    }
-}
-
-const tableCreationQuery = `CREATE TABLE IF NOT EXISTS products
-(
-    id SERIAL,
-    name TEXT NOT NULL,
-    price NUMERIC(10,2) NOT NULL DEFAULT 0.00,
-    CONSTRAINT products_pkey PRIMARY KEY (id)
-)`
-
-
-func clearTable(){
-    app.DB.Exec("DELETE FROM products")
-    app.DB.Exec("ALTER SEQUENCE products_id_seq RESTART WITH 1")
-}
